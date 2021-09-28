befc47cbc82f8804fd049e71e92544687a6f52c9
Cendy <cendyemail@gmail.com>
2021-09-28 19:38:43 +0700

    added basename to router

--- src/App.js
+++ src/App.js
@@ -5,12 +5,10 @@ import { HashRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
 	return (
-		<Router>
+		<Router basename="/">
 			<Switch>
 				<Route exact path="/" component={Home} />
-				<Route path="/manga">
-					<Manga />
-				</Route>
+				<Route path="/manga" component={Manga} />
 			</Switch>
 		</Router>
 	);
