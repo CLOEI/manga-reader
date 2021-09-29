99ad543b8782a898a0fdd8737c8592f9b8bee9ab
Cendy <cendyemail@gmail.com>
2021-09-29 12:48:28 +0700

    homepage url modified

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { HashRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
 	return (
-		<Router basename={process.env.PUBLIC_URL}>
+		<Router>
 			<Switch>
 				<Route exact path="/" component={Home} />
 				<Route path="/manga" component={Manga} />
