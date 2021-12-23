92876d70ee2cafdec5237f0ad1921ff178435e0b
Cendy <57063107+CLOEI@users.noreply.github.com>
2021-12-23 17:25:59 +0700

    Change output dir name

--- webpack.config.js
+++ webpack.config.js
@@ -75,7 +75,7 @@ module.exports = {
   },
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'build'),
+    path: path.resolve(__dirname, 'public'),
   },
   plugins: [
     new HTMLWebpackPlugin({
