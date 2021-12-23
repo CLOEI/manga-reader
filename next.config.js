3d53aaaafafb603fdf5bb1ef817c0d3267e0f50b
Cendy <cendyemail@gmail.com>
2021-12-23 18:38:31 +0700

    Test url proxy

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
 module.exports = {
-  reactStrictMode: true,
-}
+	reactStrictMode: true,
+	async redirects() {
+		return [
+			{
+				source: '/api/',
+				destination: 'https://api.mangadex.org/',
+				permanent: true,
+			},
+		];
+	},
+};
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,10 @@
 import Head from 'next/head';
 import Image from 'next/image';
-import styles from '../styles/Home.module.css';
 import axios from 'axios';
 
 export default function Home() {
 	const fetchData = async () => {
-		const data = await axios('https://api.mangadex.org/manga');
+		const data = await axios('/api/manga');
 		console.log(data);
 	};
 
